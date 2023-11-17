e1d866d9a840fca5942f9352bd2954d3627aefc5
potato0221 <wnqo10@naver.com>
2023-11-18 01:09:01 +0900

    YYJ / 2Week / 3문제 (#16)
    * YYJ-이차원 배열 대각선 순회하기
    
    * YYJ-약수의 합
    
    * [PGS] / 이차원 배열 대각선 순회하기
    
    * [PGS] / 약수의 합
    
    * [PGS]최대공약수와 최소공배수
    
    * [PGS] / n의 배수 고르기
    
    * [PGS] / 최대공약수와 최소공배수
    
    * [PGS] / 유한소수 판별하기
    
    * [PGS] / 소수 만들기
    
    * [PGS] / 문자열 정렬하기
    
    * 2주차삭제
    
    * [PGS] / 문자열 정렬하기(1)
    
    * [PGS]문자열 정렬하기(1)
    
    * [PSG] / 문자열 정렬하기(1)
    
    * [PGS] / K번째수
    
    * [PGS] / 최소직사각형

---
+++ YYJ/week2/문자열 정렬하기(1).java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer =my_string.chars() // my_string을 int의 스트림으로 변환
+                .filter(c -> c<65) // 아스키코드 65 미만인 값만 가져오도록 필터링
+                .map(c -> c-48) // 값에서 48을 빼주고 저장함 (아스키코드 0 값이 48)
+                .sorted()
+                .toArray(); //IntStream을 int 배열로 변환
+        return answer;
+        
+    }
+}
